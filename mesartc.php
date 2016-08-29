27e597c8e83a717acc574934d844b4619d0cfd79
protldnetwork <protldnetwork@yandex.com>
2016-08-29 16:22:49 +0200

    Commit #4

---
+++ mesartc.php
@@ -0,0 +1,15 @@
+<?php
+/*
+     @author: protldnetwork
+     @license: open-source
+     @name: mesartc
+     @version: v1.0.0-beta
+*/
+namespace mesartc;
+class mesartc {
+	$repo['version']='v1.0.0-beta';
+	public function info() {
+		echo 'version:'.$repo['version'];
+	}
+}
+?>
